b86f4858c4f6a13899c8c3665cde41656a8ee24f
Francesco Scarlata <francescoscarl93@gmail.com>
2020-09-13 10:18:10 +0200

    Testing the script for the general portfolio
    - added a script that would handle the general portfolio
    - this way, i can after make different scripts that can give different input based on the actual page to make
    - updated the portfolio style file with some changes
    - fixed the project info json file

---
+++ javascripts/generalProjAdder.js
@@ -0,0 +1,8 @@
+import { loadDoc } from './projectAdder.js';
+
+function loadGeneral(){
+	alert("loadGeneral");
+	loadDoc(general);
+}
+
+loadGeneral();
--- javascripts/projectAdder.js
+++ javascripts/projectAdder.js
@@ -1,8 +1,26 @@
 var path= "../resources/jsons/projectInfos.json";
+/* testing  */
+
+var jsonString=	'{"projects" : ['+
+
+	'{ "title": "Robodoom", "date": "July 2020", "linkImage": "resources/images/robodoom01.png", '+
+	'"description": "This game was made during the ...", "externalLink": "https://reaperscarl.itch.io/robo-doom" , "projectType" : "games",'+ '"projectPriority": "low", "video":"" },'+
+
+	'{ "title": "Diandao", "date": "July 2020", "linkImage": "resources/images/diandao01.png", '+
+	'"description": "This game was made during the ...", "externalLink": "https://polimi-game-collective.itch.io/diandao" ,"projectType" : "games", '+
+	'"projectPriority": "high", "video":"" },'+
+
+	'{ "title": "Uno-Java", "date": "July 2020", "linkImage": "resources/images/robodoom01.png",'+
+	'"description": "This game was made during the ...", "externalLink": " " ,"projectType" : "programs",'+
+	'"projectPriority": "high", "video":"" }'+
+']}';
+
+
 var jsonObj;
 
 /* This method will open the doc in the path and then it will parse it as json*/
-function loadDoc() {
+function loadDoc(portfolioType) {
+	/*
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -11,48 +29,47 @@ function loadDoc() {
   };
   xhttp.open("GET", path, true);
   xhttp.send();
+  */
+  alert("i've been called with: "+portfolioType);
+  loadJson(this,portfolioType);
 }
 
 /* This method will load the text and it will be used to parse it as jsonObj.
 	This will also call the for the correct order to show
   */
-function LoadJson(xml) {
-	var text= xml.responseText;
-	alert("text pre parse: "+text);
-	jsonObj= JSON.parse(text);
-	alert("projects length: "+jsonObj.projects.length);
-
+function loadJson(xml,portfolioType) {
+	
+	/*var text= xml.responseText;
+	alert("text pre parse: "+text);*/
+	
+	jsonObj= JSON.parse(jsonString);
 	if(document.title=="FS Portfolio" ){
-		GetPorfolio();
+		getPorfolio(portfolioType);
 	
 	}
 	else{
 		console.log(" this is not the general portfoliopage");
 	}
-  /*
-  var table="<tr><th>Artist</th><th>Title</th></tr>";
-
-  var x = xmlDoc.getElementsByTagName("CD");
-  for (i = 0; i <x.length; i++) { 
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-    "</td></tr>";
-  }
-  document.getElementById("demo").innerHTML = table;
-  */
 }
 /* This method is called to make the sections inside the portfolio. For now it's just the general porfolio with all the elements in random */
 
-function GetPorfolio(){
+function getPorfolio(portfolioType){
+	
+	alert(portfolioType);
 	
 	console.log("projects: "+jsonObj.projects.length);
-	var projectParent= document.getElementsByClassName("projects")[0];
-	var i;
+	var highProjParent= document.getElementById("highProj");
+	var lowProjParent= document.getElementById("lowProj");
+	
 	var section;
+	var extlink;
 	var img
 	var innerDiv;
+	var spanTitle;
+	var spanDate;
+	var spanType;
+	
+	var i;
 	for(i=0; i<jsonObj.projects.length; i++){
 		
 		section= document.createElement("section");
@@ -62,14 +79,47 @@ function GetPorfolio(){
 		img.setAttribute("class","projectImage");
 		img.setAttribute("src", jsonObj.projects[i].linkImage);
 		img.setAttribute("alt", jsonObj.projects[i].title);
-		section.appendChild(img);
 		
+		if(jsonObj.projects[i].externalLink.length>1){
+			extlink=document.createElement("a");
+			extlink.setAttribute("href", jsonObj.projects[i].externalLink);
+			extlink.setAttribute("target", "_blank");
+			extlink.appendChild(img);
+			section.appendChild(extlink);
+		}
+		else{
+			
+			section.appendChild(img);
+		}
+
 		innerDiv= document.createElement("div");
-		innerDiv.innerHTML= "<span>"+ jsonObj.projects[i].title +"</span> <br> <span> "+ jsonObj.projects[i].date+"</span>";
+		innerDiv.setAttribute("class", "infoDiv");
+		spanTitle= document.createElement("span");
+		spanTitle.setAttribute("class","projectTitle");
+		spanTitle.innerHTML=jsonObj.projects[i].title;
+		
+		spanDate= document.createElement("span");
+		spanDate.setAttribute("class","projectDate");
+		spanDate.innerHTML=jsonObj.projects[i].date;
+		
+		spanType= document.createElement("span");
+		spanType.setAttribute("class","projectType");
+		spanType.innerHTML=jsonObj.projects[i].projectType;
+		
+		innerDiv.appendChild(spanTitle);
+		innerDiv.appendChild(document.createElement("br"));
+		innerDiv.appendChild(spanDate);
+		innerDiv.appendChild(document.createElement("br"));
+		innerDiv.appendChild(spanType);
+		
 		section.appendChild(innerDiv);
-		projectParent.appendChild(section);
-	}
-	
+		if(jsonObj.projects[i].projectPriority=="high"){
+			highProjParent.appendChild(section);
+		}
+		else{
+			lowProjParent.appendChild(section);
+		}	
+	}	
 }
 
-loadDoc();
+export { loadDoc };
